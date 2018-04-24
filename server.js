10d62360464b5fd116d31fffafe27fd12c94aaa2
graemeerickson <graemeerickson@gmail.com>
2018-04-24 16:35:39 -0700

    Required additional modules. Added routes, including for user auth.

---
+++ server.js
@@ -0,0 +1,64 @@
+// include modules
+require('dotenv').config(); // loads the .env
+const bodyParser = require('body-parser');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const isLoggedIn = require('./middleware/isLoggedIn');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const passport = require('./config/passportConfig');
+const path = require('path');
+const session = require('express-session');
+
+// initialize app
+const app = express();
+
+/* error logger, static routes */
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// set and use statements. set view engine and use middleware.
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(expressLayouts);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// just a convenience, but makes life easier...
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
+});
+
+// top-level routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
+
+// include any routes from controllers
+app.use('/auth', require('./controllers/auth'));
+
+/* Error Handling */
+app.get('*', function (req, res) {
+    res.status(404).send('This is not the page you are looking for')
+});
+
+/* Listen on PORT */
+app.set('port', process.env.PORT)
+
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port: ${app.get('port')}`)
+})
