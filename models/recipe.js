34ae75ca9f89335fa2d21c4abc0dadf83fed87c4
graemeerickson <graemeerickson@gmail.com>
2018-04-25 14:42:19 -0700

    Updated Recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Ingredient = require('./ingredient.js')
+
+const recipeSchema = new Schema({
+  recipeName: String,
+  servingSize: Number,
+  ingredients: [ Ingredient.schema ],
+  ownedBy: String,
+  active: Boolean
+});
+
+const Recipe = mongoose.model('Recipe',recipeSchema);
+
+module.exports = Recipe;
