83e3ad3acab6f77099278d2980d8f0cdeae6ff12
graemeerickson <graemeerickson@gmail.com>
2018-04-25 14:40:11 -0700

    Updated seed code to align with latest schemas

--- seed.js
+++ seed.js
@@ -1,59 +1,89 @@
+// var db = require("./models");
 var db = require("./models");
+// var ObjectId = require('mongodb').ObjectID;
+// var ObjectId = require('mongoose').Schema.ObjectId;
+var ObjectId = require('mongoose').Types.ObjectId;
 
-var sampleMealPlan = [
-  {
-    dayOfWeek: 'Sunday'
-  },
-  {
-    dayOfWeek: 'Monday'
-  },
-  {
-    dayOfWeek: 'Tuesday'
-  },
-  {
-    dayOfWeek: 'Wednesday'
-  },
-  {
-    dayOfWeek: 'Thursday'
-  },
-  {
-    dayOfWeek: 'Friday'
-  },
-  {
-    dayOfWeek: 'Saturday'
-  }
-];
-console.log('sampleMealPlan:', sampleMealPlan);
+// var sampleMealPlan = [
+//   {
+//     dayOfWeek: 'Sunday'
+//   },
+//   {
+//     dayOfWeek: 'Monday'
+//   },
+//   {
+//     dayOfWeek: 'Tuesday'
+//   },
+//   {
+//     dayOfWeek: 'Wednesday'
+//   },
+//   {
+//     dayOfWeek: 'Thursday'
+//   },
+//   {
+//     dayOfWeek: 'Friday'
+//   },
+//   {
+//     dayOfWeek: 'Saturday'
+//   }
+// ];
+// console.log('sampleMealPlan:', sampleMealPlan);
 
+// POPULATE RECIPES
 var sampleRecipes = [];
 
 sampleRecipes.push({ recipeName: 'breakfastRecipe' });
 sampleRecipes.push({ recipeName: 'lunchRecipe' });
 sampleRecipes.push({ recipeName: 'dinnerRecipe' });
 
-var sampleIngredients = 'apple';
+// var sampleIngredients = 'apple';
 
 sampleRecipes.forEach( (recipe) => {
   recipe.servingSize = 1,
-  recipe.ingredients = {sampleIngredients},
-  recipe.selected = true
+  recipe.ingredients = ['apple'],
+  recipe.ownedBy = '5adfc0b829ea87c828775298',
+  recipe.active = 'FALSE'
 });
 
 console.log('sampleRecipes:', sampleRecipes);
 
-sampleMealPlan.forEach( (day) => {
-  day.recipes = sampleRecipes;
+// POPULATE USER RECORD WITH RECIPE IDs
+
+// db.User.find({}, function(err) {
+//   console.log('entered findOne');
+//   // console.log(user);
+// })
+
+
+// db.User.findById(id, function(err, user) {
+//   console.log('entered findOne');
+//   if (err) return console.log('err:', err);
+//   res.send('entered findOne');
+//   // console.log(user);
+// })
+
+// db.User.findOne({_id: id}, function(err, user) {
+//   console.log('entered findOne');
+//   if (err) return console.log('err:', err);
+//   res.send('entered findOne');
+//   // console.log(user);
+// })
+
+db.User.findOneAndUpdate({ _id: '5adfc0b829ea87c828775298'}, { recipes: {sampleRecipes} }, function(err, user) {
+  console.log('entered findOneAndUpdate');
+  if (err) return console.log('err:', err);
+  // res.send(user);
+  // console.log('User:', user);
 });
 
-console.log('sampleMealPlan:', sampleMealPlan);
+// REMOVE EXISTING MEAL PLAN AND CREATE NEW ONE
+// db.MealPlan.remove({}, function(err, mealPlan){
 
-db.MealPlan.remove({}, function(err, mealPlan){
+//   db.MealPlan.create(sampleMealPlan, function(err, mealPlan){
+//     if (err) { return console.log('ERROR', err); }
+//     console.log("all mealPlans:", mealPlan);
+//     console.log("created", mealPlan.length, "mealPlan");
+//     process.exit();
+//   });
 
-  db.MealPlan.create(sampleMealPlan, function(err, mealPlan){
-    if (err) { return console.log('ERROR', err); }
-    console.log("all mealPlans:", mealPlan);
-    console.log("created", mealPlan.length, "mealPlan");
-    process.exit();
-  });
-
-});
+// });
