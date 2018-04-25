e27c6844f6587b2106cb62acb7690cac34ca7c72
graemeerickson <graemeerickson@gmail.com>
2018-04-25 16:01:55 -0700

    Modified mealplan schema

--- models/mealplan.js
+++ models/mealplan.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Recipe = require('./recipe.js')
-const Ingredient = require('./ingredient.js')
 
 const mealPlanSchema = new Schema({
-  dayOfWeek: String,
-  recipes: [ Recipe.schema ]
+  mealPlan: [ String ]
 });
 
 const MealPlan = mongoose.model('MealPlan',mealPlanSchema);
