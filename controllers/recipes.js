11297bb1df626e5c94205a6cde57125b38be8749
graemeerickson <graemeerickson@gmail.com>
2018-04-26 16:15:13 -0700

    Added functionality to insert new recipe into user record in db

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -16,16 +16,65 @@ router.get('/', isLoggedIn, (req, res) => {
 
 // create a new recipe belonging to the user
 router.post('/', isLoggedIn, (req, res) => {
+  console.log('got to recipes POST backend route');
   // create new ingredients object
-  console.log(req.body);
+  console.log('req.body:', req.body);
+  
+  // req:
+    // { recipeNameInput: 'first recipe',
+    // ingredientQtyInput: '1',
+    // ingredientMeasuringUnitSelect: 'cup',
+    // ingredientItemInput: 'sugar',
+    // prepInstructionsTextArea: 'prep',
+    // servingSizeSelect: '1' }
+
+  // convert entered strings into arrays
+  // let ingredientItemArr = req.body.ingredientItemInput.split(',');
+  // let ingredientQtyArr = req.body.ingredientQtyInput.split(',');
+  // let ingredientMeasuringUnitArr = req.body.ingredientMeasuringUnitSelect.split(',');
+  let ingredientItemArr = [];
+  ingredientItemArr.push(req.body.ingredientItemInput);
+  
+  let ingredientQtyArr = [];
+  ingredientQtyArr.push(req.body.ingredientQtyInput);
+
+  let ingredientMeasuringUnitArr = [];
+  ingredientMeasuringUnitArr.push(req.body.ingredientMeasuringUnitSelect);
+
+  let ingredientsArr = [];
+  
+  for (let i = 0; i < ingredientItemArr.length; i++) {
+    let tmpIngredientObj = {};
+    tmpIngredientObj.ingredientName = req.body.ingredientItemInput[i];
+    tmpIngredientObj.qty = req.body.ingredientQtyInput[i];
+    tmpIngredientObj.measuringUnit = req.body.ingredientMeasuringUnitSelect[i];
+    ingredientsArr.push(tmpIngredientObj);
+  }
+
+  console.log('ingredientsArr:', ingredientsArr);
+
+  let newRecipe = new db.Recipe({ recipeName: req.body.recipeNameInput, servingSize: req.body.servingSizeSelect, ingredients: ingredientsArr, prepInstructions: req.body.prepInstructionsTextArea, activeCount: 0 })
+  newRecipe.save();
+  console.log('newRecipe:', newRecipe);
+
+  // let albumId = req.params.album_id.slice(1, req.params.album_id.length);
+  db.User.findByIdAndUpdate(
+    res.locals.currentUser,
+    {$push: {recipes: newRecipe}},
+    { 'new': true },
+    function (err, user) {
+      if (err) {
+        console.log('Error adding new recipe to user db record', err);
+      };
+      res.render('home');
+  });
   // create new recipes object, incl ingredients
   // redirect to homepage
 
   // db.User.find()
   // res.json(userRecipes);
-  console.log('got to recipes POST backend route');
-  res.send('got to recipes POST backend route');
-  res.json()
+  
+  // res.redirect('/');
 })
 
 // allow other files to access the routes defined here
