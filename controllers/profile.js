5ae88a5a3bdcdae131c35f2087903e25d72d16bd
graemeerickson <graemeerickson@gmail.com>
2018-04-27 13:45:06 -0700

    Updated edit recipe form

--- controllers/profile.js
+++ controllers/profile.js
@@ -6,12 +6,28 @@ const router = express.Router();
 // include the user and recipe models
 const db = require('../models');
 
+var recipeId;
+
 router.get('/', isLoggedIn, (req, res) => {
   db.User.findById(res.locals.currentUser, function(err, currentUser) {
     if (err) return res.send(err);
     res.render('profile', {userRecipes: currentUser.recipes});
   });
 });
+
+router.post('/editRecipe', isLoggedIn, (req, res) => {
+  recipeId = req.body.id;
+  res.json(recipeId);
+});
+
+router.get('/editRecipe', isLoggedIn, (req, res) => {
+  db.User.findById(res.locals.currentUser._id, function(err, user) {
+    const userRecipe = user.recipes.filter( (recipe) => {
+      return recipe._id == recipeId;
+    })
+    res.render('editRecipe', {userRecipe: userRecipe});
+  })
+})
 
 // allow other files to access the routes defined here
 module.exports = router;
