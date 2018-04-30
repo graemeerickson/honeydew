d5e6f2fc5b8cd61eae0f153dba1898198e98d3d2
graemeerickson <graemeerickson@gmail.com>
2018-04-29 22:34:18 -0700

    Converted if/elseif/elseif statement to switch statement for readability. Cleaned up console logs and comments.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,15 +1,15 @@
 const cloudinary = require('cloudinary');
 const express = require('express');
 const isLoggedIn = require('../middleware/isLoggedIn');
-const multer = require('multer');
+const multer = require('multer');    // for cloudinary
 const passport = require('../config/passportConfig');
 const router = express.Router();
-const upload = multer({ dest: './uploads/' });
+const upload = multer({ dest: './uploads/' });  // for cloudinary
 
 // include the user and recipe models
 const db = require('../models');
 
-// get all of the user's recipes
+// GET route to get all of the user's recipes
 router.get('/', isLoggedIn, (req, res) => {
   // find user's recipe list
   const user = db.User.find({ _id: res.locals.currentUser}, function (err, user) {
@@ -17,10 +17,8 @@ router.get('/', isLoggedIn, (req, res) => {
   })
 });
 
-// create a new recipe belonging to the user
-router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {  
-  console.log('req.body', req.body);
-  console.log('typeof req.body.ingredientItemInput:', typeof req.body.ingredientItemInput)
+// POST route to create a new recipe
+router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
   let ingredientsArr = [];
   // if just one ingredient in recipe, convert ingredient strings to arrays
   if (typeof req.body.ingredientItemInput === 'string') {
@@ -54,7 +52,6 @@ router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
   if (req.file !== undefined) {
     // create new recipe object with Cloudinary image url, then insert into user record. redirect user to homepage.
     cloudinary.uploader.upload(req.file.path, function(result) {
-      console.log('running cloudinary uploader');
       let newRecipe = new db.Recipe({ recipeName: req.body.recipeNameInput, servingSize: req.body.servingSizeSelect, ingredients: ingredientsArr, prepInstructions: req.body.prepInstructionsTextArea, prepTime: req.body.prepTimeInput, cookTime: req.body.cookTimeInput, mealType: req.body.mealTypeSelect, imgUrl: result.url, activeCount: 0 })
       newRecipe.save();
 
@@ -88,59 +85,60 @@ router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
 
 // PUT route to update user's recipe & mealplan
 router.put('/', isLoggedIn, (req,res) => {
-  console.log('req.body:', req.body);
   let scenario = req.body.scenario;
 
   // scenario: new recipe is selected, and mealplan slot is blank - UPDATE CURRENT RECIPE AND POPULATE MEAL PLAN SLOT
-  if (scenario === 'clear-mealplan-slot') {
-    db.User.findById(res.locals.currentUser.id, function(err, user) {
-      if (err) { console.log("Error finding user in db", err); };
-      // decrement cleared recipe's active count
-      for (let i = 0; i < user.recipes.length; i++) {
-        if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
-          user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
+  switch (scenario) {
+    case 'clear-mealplan-slot':
+      db.User.findById(res.locals.currentUser.id, function(err, user) {
+        if (err) { console.log("Error finding user in db", err); };
+        // decrement cleared recipe's active count
+        for (let i = 0; i < user.recipes.length; i++) {
+          if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
+            user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
+          }
         }
-      }
-      // update mealplan slot to a blank value
-      user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,"");
-      user.save();
-      res.render('home');
-    })
-  }
-  // scenario: new recipe is selected, and mealplan slot is blank - UPDATE CURRENT RECIPE AND POPULATE MEAL PLAN SLOT
-  else if (scenario === 'populate-mealplan-slot') {
-    db.User.findById(res.locals.currentUser.id, function(err, user) {
-      if (err) { console.log("Error finding user in db", err); };
-      // increment selected recipe's active count
-      for (let i = 0; i < user.recipes.length; i++) {
-        if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
-          user.recipes[i].activeCount += 1;
+        // update mealplan slot to a blank value
+        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,"");
+        user.save();
+        res.render('home');
+      })
+      break;
+    // scenario: new recipe is selected, and mealplan slot is blank - UPDATE CURRENT RECIPE AND POPULATE MEAL PLAN SLOT
+    case 'populate-mealplan-slot':
+      db.User.findById(res.locals.currentUser.id, function(err, user) {
+        if (err) { console.log("Error finding user in db", err); };
+        // increment selected recipe's active count
+        for (let i = 0; i < user.recipes.length; i++) {
+          if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
+            user.recipes[i].activeCount += 1;
+          }
         }
-      }
-      // update mealplan slot to selected recipe
-      user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
-      user.save();
-      res.render('home');
-    })
-  }
-  // scenario: new recipe is selected, and mealplan slot is not blank - UPDATE PREVIOUS AND CURRENT RECIPE, AND REPLACE MEAL PLAN SLOT
-  else if (scenario === 'replace-mealplan-slot') {
-    db.User.findById(res.locals.currentUser.id, function(err, user) {
-      if (err) { console.log("Error finding user in db", err); };
-      // decrement previous recipe's active count, and increment selected recipe's active count
-      for (let i = 0; i < user.recipes.length; i++) {
-        if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
-          user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
+        // update mealplan slot to selected recipe
+        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
+        user.save();
+        res.render('home');
+      })
+      break;
+    // scenario: new recipe is selected, and mealplan slot is not blank - UPDATE PREVIOUS AND CURRENT RECIPE, AND REPLACE MEAL PLAN SLOT
+    case 'replace-mealplan-slot':
+      db.User.findById(res.locals.currentUser.id, function(err, user) {
+        if (err) { console.log("Error finding user in db", err); };
+        // decrement previous recipe's active count, and increment selected recipe's active count
+        for (let i = 0; i < user.recipes.length; i++) {
+          if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
+            user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
+          }
+          if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
+            user.recipes[i].activeCount += 1;
+          }
         }
-        if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
-          user.recipes[i].activeCount += 1;
-        }
-      }
-      // update mealplan slot to selected recipe
-      user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
-      user.save();
-      res.render('home');
-    })
+        // update mealplan slot to selected recipe
+        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
+        user.save();
+        res.render('home');
+      })
+      break;
   }
 })
 // allow other files to access the routes defined here
