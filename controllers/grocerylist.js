f404f82cfbe11accb6c4a36a157ea0fb984db252
graemeerickson <graemeerickson@gmail.com>
2018-04-29 22:25:28 -0700

    Cleaned up code, added comments

--- controllers/grocerylist.js
+++ controllers/grocerylist.js
@@ -3,16 +3,16 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 const passport = require('../config/passportConfig');
 const router = express.Router();
 
-// include the user and recipe models
+// include the user model
 const db = require('../models');
 
 router.get('/', isLoggedIn, (req, res) => {
   db.User.findById(res.locals.currentUser, function(err, currentUser) {
     if (err) return res.send(err);
+    // find only the recipes added to the user's mealplan
     let activeRecipes = currentUser.recipes.filter( (recipe) => {
       return recipe.activeCount > 0;
     });
-    // res.render('grocerylist', {userRecipes: currentUser.recipes});
     res.render('grocerylist', {userRecipes: activeRecipes});
   });
 });
