83e7355b073288b4af18e6a28a9fa9e1678f1c41
graemeerickson <graemeerickson@gmail.com>
2018-04-29 22:41:13 -0700

    Cleaned up comments

--- models/index.js
+++ models/index.js
@@ -1,11 +1,6 @@
 // the mongoose.connect line needs to happen exactly once in your code
 const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/honeydew');
-// if (process.env.NODE_ENV == "production") {
-//   mongoose.connect(process.env.MLAB_URL)
-// } else {
-//   mongoose.connect("mongodb://localhost/honeydew");
-// }
 
 module.exports.User = require("./user.js");
 module.exports.Ingredient = require("./ingredient.js");
