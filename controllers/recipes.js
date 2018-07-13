7636433f2435e631d049ff5a8f31fd2dd0a67671
graemeerickson <graemeerickson@gmail.com>
2018-07-13 14:36:23 -0700

    Revised variable formatting. Moved from bracket to dot notation to access req.body data. Set default placeholder recipe image if none is provided. Added GET and DELETE routes to show and remove specific recipe by ID.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -18,16 +18,16 @@ router.get('/', isLoggedIn, (req, res) => {
 });
 
 // POST route to create a new recipe
-router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
+router.post('/', isLoggedIn, upload.single('my-file'), (req, res) => {
   let ingredientsArr = [];
   // if just one ingredient in recipe, convert ingredient strings to arrays
-  if (typeof req.body.ingredientItemInput === 'string') {
+  if (typeof req.body['ingredient-item-input'] === 'string') {
     let ingredientItemArr = [];
-    ingredientItemArr.push(req.body.ingredientItemInput);
+    ingredientItemArr.push(req.body['ingredientItemInput']);
     let ingredientQtyArr = [];
-    ingredientQtyArr.push(req.body.ingredientQtyInput);
+    ingredientQtyArr.push(req.body['ingredientQtyInput']);
     let ingredientMeasuringUnitArr = [];
-    ingredientMeasuringUnitArr.push(req.body.ingredientMeasuringUnitSelect);
+    ingredientMeasuringUnitArr.push(req.body['ingredientMeasuringUnitSelect']);
 
     // populate ingredient objects & push to a single consolidated ingredients array
     for (let i = 0; i < ingredientItemArr.length; i++) {
@@ -40,11 +40,11 @@ router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
   }
   // multiple ingredients included, so no need to convert to array
   else {
-    for (let i = 0; i < req.body.ingredientItemInput.length; i++) {
+    for (let i = 0; i < req.body['ingredient-item-input'].length; i++) {
       let tmpIngredientObj = {};
-      tmpIngredientObj.ingredientName = req.body.ingredientItemInput[i];
-      tmpIngredientObj.qty = req.body.ingredientQtyInput[i];
-      tmpIngredientObj.measuringUnit = req.body.ingredientMeasuringUnitSelect[i];
+      tmpIngredientObj.ingredientName = req.body['ingredient-item-input'][i];
+      tmpIngredientObj.qty = req.body['ingredient-qty-input'][i];
+      tmpIngredientObj.measuringUnit = req.body['ingredient-measuring-unit-select'][i];
       ingredientsArr.push(tmpIngredientObj);
     }
   };
@@ -52,7 +52,7 @@ router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
   if (req.file !== undefined) {
     // create new recipe object with Cloudinary image url, then insert into user record. redirect user to homepage.
     cloudinary.uploader.upload(req.file.path, function(result) {
-      let newRecipe = new db.Recipe({ recipeName: req.body.recipeNameInput, servingSize: req.body.servingSizeSelect, ingredients: ingredientsArr, prepInstructions: req.body.prepInstructionsTextArea, prepTime: req.body.prepTimeInput, cookTime: req.body.cookTimeInput, mealType: req.body.mealTypeSelect, imgUrl: result.url, activeCount: 0 })
+      let newRecipe = new db.Recipe({ recipeName: req.body['recipe-name-input'], servingSize: req.body['serving-size-select'], ingredients: ingredientsArr, prepInstructions: req.body['prep-instructions-text-area'], prepTime: req.body['prep-time-input'], cookTime: req.body['cook-time-input'], mealType: req.body['meal-type-select'], imgUrl: result.url, activeCount: 0 })
       newRecipe.save();
 
       db.User.findByIdAndUpdate(
@@ -68,7 +68,7 @@ router.post('/', isLoggedIn, upload.single('myFile'), (req, res) => {
   } 
   // no image included, so bypass cloudinary
   else {
-    let newRecipe = new db.Recipe({ recipeName: req.body.recipeNameInput, servingSize: req.body.servingSizeSelect, ingredients: ingredientsArr, prepInstructions: req.body.prepInstructionsTextArea, prepTime: req.body.prepTimeInput, cookTime: req.body.cookTimeInput, mealType: req.body.mealTypeSelect, imgUrl: '', activeCount: 0 })
+    let newRecipe = new db.Recipe({ recipeName: req.body['recipe-name-input'], servingSize: req.body['serving-size-select'], ingredients: ingredientsArr, prepInstructions: req.body['prep-instructions-text-area'], prepTime: req.body['prep-time-input'], cookTime: req.body['cook-time-input'], mealType: req.body['meal-type-select'], imgUrl: 'https://res.cloudinary.com/graemeerickson/image/upload/v1527094309/recipe-placeholder-image.png', activeCount: 0 })
       newRecipe.save();
 
     db.User.findByIdAndUpdate(
@@ -94,12 +94,12 @@ router.put('/', isLoggedIn, (req,res) => {
         if (err) { console.log("Error finding user in db", err); };
         // decrement cleared recipe's active count
         for (let i = 0; i < user.recipes.length; i++) {
-          if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
+          if (user.recipes[i].recipeName === req.body['selected-meal-plan-slot-existing-recipe']) {
             user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
           }
         }
         // update mealplan slot to a blank value
-        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,"");
+        user.mealPlan.splice(req.body['selected-meal-plan-slot-id'],1,"");
         user.save();
         res.render('home');
       })
@@ -110,12 +110,12 @@ router.put('/', isLoggedIn, (req,res) => {
         if (err) { console.log("Error finding user in db", err); };
         // increment selected recipe's active count
         for (let i = 0; i < user.recipes.length; i++) {
-          if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
+          if (user.recipes[i].recipeName === req.body['selected-recipe-name']) {
             user.recipes[i].activeCount += 1;
           }
         }
         // update mealplan slot to selected recipe
-        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
+        user.mealPlan.splice(req.body['selected-meal-plan-slot-id'],1,req.body['selected-recipe-name']);
         user.save();
         res.render('home');
       })
@@ -126,20 +126,50 @@ router.put('/', isLoggedIn, (req,res) => {
         if (err) { console.log("Error finding user in db", err); };
         // decrement previous recipe's active count, and increment selected recipe's active count
         for (let i = 0; i < user.recipes.length; i++) {
-          if (user.recipes[i].recipeName === req.body.selectedMealPlanSlotExistingRecipe) {
+          if (user.recipes[i].recipeName === req.body['selected-meal-plan-slot-existing-recipe']) {
             user.recipes[i].activeCount > 0 ? user.recipes[i].activeCount -= 1 : user.recipes[i].activeCount = 0;
           }
-          if (user.recipes[i].recipeName === req.body.selectedRecipeName) {
+          if (user.recipes[i].recipeName === req.body['selected-recipe-name']) {
             user.recipes[i].activeCount += 1;
           }
         }
         // update mealplan slot to selected recipe
-        user.mealPlan.splice(req.body.selectedMealPlanSlotId,1,req.body.selectedRecipeName);
+        user.mealPlan.splice(req.body['selected-meal-plan-slot-id'],1,req.body['selected-recipe-name']);
         user.save();
         res.render('home');
       })
       break;
   }
+});
+
+router.get('/:id', isLoggedIn, (req, res) => {
+  db.User.findById(res.locals.currentUser._id, function(err, user) {
+    let userRecipe = user.recipes.id(req.params.id);
+    console.log('userRecipe:', userRecipe);
+    res.render('recipes/show', {userRecipe: userRecipe});
+  });
+});
+
+// DELETE route to remove selected recipe from the db
+router.delete('/:id', isLoggedIn, (req, res) => {
+  let recipeIdToRemove = req.params.id;
+  db.User.findById(res.locals.currentUser._id, function(err, user) {
+    if (err) { console.log("Error finding user in db", err) };
+    user.recipes.forEach( (recipe) => {
+      if (recipe.id === recipeIdToRemove) {
+        recipe.remove();
+      }
+    });
+    user.save();
+    res.status(200).send();
+    res.redirect('profile');
+    // user.recipes.findByIdAndDelete(req.params.id, function(error, recipe) { 
+    //   if (error) { console.log("Error finding user's recipe in db", error) };
+    //   user.save();
+    //   res.redirect('profile');
+    // })
+  })
 })
+
 // allow other files to access the routes defined here
 module.exports = router;
